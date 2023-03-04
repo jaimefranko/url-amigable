e24900a929522db7cba599ea12f1994b0ddd0a1a
Jaime Franco <jaimefranko1@gmail.com>
2023-03-03 18:38:26 -0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,251 @@
+<?php 
+include_once("functions.php");
+?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+	<script data-ad-client="ca-pub-5867472885387312" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0" />
+	<title>Ejemplo de url amigable con php</title>
+	<link rel="shortcut icon" href="https://jaimefranko.com/wp-content/uploads/2016/09/spinner.png" defer />
+	<!-- CSS  -->
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" defer>
+	<!-- Compiled and minified CSS -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css" defer>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-83866247-1"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', 'UA-83866247-1');
+	</script>
+	<meta name="description" content="Blog de Noticias, Invertir, Bitcoin, Tecnología, Freenlacer, Apps, PSD, Flyers, Diseño Grafico, Marketing, SEO, Video Juegos" />
+	<meta name="robots" content="max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
+	<link rel="canonical" href="https://jaimefranko.com/" />
+	<meta property="og:locale" content="es_ES" />
+	<meta property="og:type" content="website" />
+	<meta property="og:title" content="Blog de tecnologia, seo, marketing, diseño, apps, bitcoin | Jaime Franko" />
+	<meta property="og:description" content="Blog de Noticias, Invertir, Bitcoin, Tecnología, Freenlacer, Apps, PSD, Flyers, Diseño Grafico, Marketing, SEO, Video Juegos" />
+	<meta property="og:url" content="https://jaimefranko.com/" />
+	<meta property="og:site_name" content="Jaime Franko Blog" />
+	<meta property="fb:app_id" content="1760805120874258" />
+	<meta property="og:image" content="https://jaimefranko.com/wp-content/uploads/2016/09/Facebook_Cover.png" />
+	<meta property="og:image:secure_url" content="https://jaimefranko.com/wp-content/uploads/2016/09/Facebook_Cover.png" />
+	<meta property="og:image:width" content="851" />
+	<meta property="og:image:height" content="315" />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta name="twitter:description" content="Blog de Noticias, Invertir, Bitcoin, Tecnología, Freenlacer, Apps, PSD, Flyers, Diseño Grafico, Marketing, SEO, Video Juegos" />
+	<meta name="twitter:title" content="Blog de tecnologia, seo, marketing, diseño, apps, bitcoin | Jaime Franko" />
+	<meta name="twitter:site" content="@jaimefrankoweb" />
+	<meta name="twitter:image" content="https://jaimefranko.com/wp-content/uploads/2016/09/Facebook_Cover.png" />
+	<meta name="twitter:creator" content="@jaimefrankoweb" />
+	<script type='application/ld+json' class='yoast-schema-graph yoast-schema-graph--main'>
+		{
+			"@context": "https://schema.org",
+			"@graph": [{
+				"@type": ["Person", "Organization"],
+				"@id": "https://jaimefranko.com/#/schema/person/395717fb4c234129c715857fc6011709",
+				"name": "jaimefranko",
+				"image": {
+					"@type": "ImageObject",
+					"@id": "https://jaimefranko.com/#personlogo",
+					"url": "https://secure.gravatar.com/avatar/87539fcb67e41dd003bd967a308f5d5e?s=96&d=mm&r=g",
+					"caption": "jaimefranko"
+				},
+				"logo": {
+					"@id": "https://jaimefranko.com/#personlogo"
+				},
+				"description": "\u00a1Hola! mi nombre es Jaime Franco soy Full Stack Developer, desde el 2010 estoy creando proyectos en diferentes \u00e1reas de este maravilloso mundo digital, desde entonces puedo decir que cada d\u00eda se aprende algo nuevo.",
+				"sameAs": ["http://facebook.com/jaimefranko", "http://instagram.com/jaimefrankoweb", "https://www.pinterest.com/jaimefranko/", "https://twitter.com/jaimefrankoweb", "https://www.youtube.com/channel/UC3uQr4yfceUl0_l2WjAqQmA"]
+			}, {
+				"@type": "WebSite",
+				"@id": "https://jaimefranko.com/#website",
+				"url": "https://jaimefranko.com/",
+				"name": "Jaime Franko",
+				"description": "Noticias, Invertir, Bitcoin, Tecnolog\u00eda, Freenlacer, Apps",
+				"publisher": {
+					"@id": "https://jaimefranko.com/#/schema/person/395717fb4c234129c715857fc6011709"
+				},
+				"potentialAction": {
+					"@type": "SearchAction",
+					"target": "https://jaimefranko.com/?s={search_term_string}",
+					"query-input": "required name=search_term_string"
+				}
+			}, {
+				"@type": "ImageObject",
+				"@id": "https://jaimefranko.com/#primaryimage",
+				"url": "https://jaimefranko.com/wp-content/uploads/2016/09/Facebook_Cover.png",
+				"width": 851,
+				"height": 315,
+				"caption": "Jaime Franko"
+			}, {
+				"@type": "WebPage",
+				"@id": "https://jaimefranko.com/#webpage",
+				"url": "https://jaimefranko.com/",
+				"inLanguage": "es",
+				"name": "Blog de tecnologia, seo, marketing, dise\u00f1o, apps, bitcoin | Jaime Franko",
+				"isPartOf": {
+					"@id": "https://jaimefranko.com/#website"
+				},
+				"about": {
+					"@id": "https://jaimefranko.com/#/schema/person/395717fb4c234129c715857fc6011709"
+				},
+				"primaryImageOfPage": {
+					"@id": "https://jaimefranko.com/#primaryimage"
+				},
+				"datePublished": "2015-12-30T15:42:59+00:00",
+				"dateModified": "2020-01-12T16:19:17+00:00",
+				"description": "Blog de Noticias, Invertir, Bitcoin, Tecnolog\u00eda, Freenlacer, Apps, PSD, Flyers, Dise\u00f1o Grafico, Marketing, SEO, Video Juegos"
+			}]
+		}
+	</script>
+
+</head>
+
+<body>
+	<nav class="blue-grey darken-4" role="navigation">
+		<div class="nav-wrapper container"><a id="logo-container" href="https://jaimefranko.com" class="brand-logo">Jaime Franko</a>
+			<ul class="right hide-on-med-and-down">
+				<li></li>
+			</ul>
+
+			<ul id="nav-mobile" class="sidenav">
+				<li></li>
+			</ul>
+			<a data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+		</div>
+	</nav>
+	<div class="section no-pad-bot" id="index-banner">
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- ejemplo_php horizontal -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-5867472885387312"
+     data-ad-slot="4717488291"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+		<div class="container">
+			
+			<br><br>
+			<h1 class="header center orange-text">Crear urls amigables con .htaccess y PHP</h1>
+			<div class="row center">
+				<h5 class="header col s12 light"></h5>
+			</div>
+
+			<div class="container center">
+				<!-- inicia codigo -->
+<?php
+if(isset($_POST["crear"])){
+			$titulo = $_POST["titulo"];
+			$contenido = htmlspecialchars($_POST["contenido"], ENT_QUOTES);
+			$link = limpiar($titulo);
+	    $id_entrada = 2;
+?>
+					Clic en la url: <a href="publicacion.php?id_entrada=<?php echo $id_entrada;?>&titulo=<?php echo $link;?>&contenido=<?php echo $contenido;?>">
+		jaimefranko.com/ejemplos_php/url-amigable-ejemplo/<?php echo $link;?></a>
+					<?php
+	}
+?>
+				<form class="col s12" method="post">
+					<div class="row">
+						<div class="input-field col s12">
+							<i class="material-icons prefix">mode_edit</i>
+							<input id="input_text" class="active" type="text" name="titulo" placeholder="Escribe el titulo de tu url" required value="<?php echo $_POST['titulo'];?>">
+							<label for="input_text">Titulo</label>
+						</div>
+					</div>
+					<div class="row">
+						<div class="input-field col s12">
+							<i class="material-icons prefix">comment</i>
+							<textarea id="textarea1" class="materialize-textarea" name="contenido" placeholder="Escribe tu contenido" required><?php echo $_POST['contenido'];?></textarea>
+							<label for="textarea1">Contenido</label>
+						</div>
+					</div>
+					<div class="row">
+
+						<button class="btn waves-effect waves-light" type="submit" name="crear">Crear
+														<i class="material-icons right">send</i>
+													</button>
+					</div>
+				</form>
+				
+					<!-- finaliza codigo -->
+			</div>
+			<br><br>
+
+		</div>
+	</div>
+
+
+	<div class="container">
+		<div class="section">
+
+			<!--   Icon Section   -->
+			<div class="row">
+				<div class="col s12 m4">
+					<div class="icon-block">
+						<h2 class="center light-blue-text"><i class="material-icons">flash_on</i></h2>
+						<h5 class="center">MySql</h5>
+
+						<p class="light">Ejemplo facil de programar en php para mejorar nuestro seo. Espera el tutorial conectado a una base de datos MySql, gracias por visitar mi blog. </p>
+					</div>
+				</div>
+
+				<div class="col s12 m4">
+					<div class="icon-block">
+						<h2 class="center light-blue-text"><i class="material-icons">group</i></h2>
+						<h5 class="center">Php</h5>
+
+						<p class="light">Contenido de php, mysql, posicionamiento web, seo, marketing, tutoriales de manejo de base de datos, php system, etc.</p>
+					</div>
+				</div>
+
+				<div class="col s12 m4">
+					<div class="icon-block">
+						<h2 class="center light-blue-text"><i class="material-icons">settings</i></h2>
+						<h5 class="center">Donativos</h5>
+
+						<p class="light center">
+							<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" class="center">
+								<input type="hidden" name="cmd" value="_s-xclick" />
+								<input type="hidden" name="hosted_button_id" value="JMVL4LZMYR42G" />
+								<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+								<img alt="" border="0" src="https://www.paypal.com/en_SV/i/scr/pixel.gif" width="1" height="1" />
+							</form>
+						</p>
+					</div>
+				</div>
+			</div>
+
+		</div>
+		<br><br>
+
+	</div>
+
+	<footer class="page-footer blue-grey darken-4">
+
+		<div class="footer-copyright">
+			<div class="container">
+				Powered By <a class="orange-text text-lighten-3" href="https://jaimefranko.com">Jaime Franko <?php echo date("Y");?></a>
+			</div>
+		</div>
+	</footer>
+
+	<!--  Scripts-->
+	<script src="https://code.jquery.com/jquery-2.1.1.min.js" async></script>
+	<!-- Compiled and minified JavaScript -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js" async></script>
+
+</body>
+
+</html>
